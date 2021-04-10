1ad094093180c83af1ed19cef7f401cc453066aa
ixaxaar <root@ixaxaar.in>
2021-04-11 00:46:13 +0530

    package install

---
+++ src/packages/install.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import glob
+import shutil
+import tempfile
+from os import path
+from typing import *
+
+import filetype
+import requests
+from git import Repo
+from pyunpack import Archive
+from tqdm import tqdm
+
+ARCHIVE_TYPES = [
+  "application/x-lzip",
+  "application/x-unix-archive",
+  "application/x-compress",
+  "application/zip",
+  "application/x-tar",
+  "application/x-rar-compressed",
+  "application/gzip",
+  "application/x-bzip2",
+  "application/x-7z-compressed",
+  "application/x-xz",
+]
+
+
+class Install:
+  where: str = path.join(path.expanduser("~"), ".refl")  # link / path
+
+  def __init__(self, where: str):
+    self.where = where
+
+  def git(self, url: str, head: str = None, tag: str = None, commit_hash: str = None) -> str:
+    # set target directory of install
+    if head is not None:
+      target_directory = path.join(self.where, f"{repo_name}-{head.name}")
+    elif tag is not None:
+      target_directory = path.join(self.where, f"{repo_name}-{tag}")
+    elif commit_hash is not None:
+      target_directory = path.join(self.where, f"{repo_name}-{commit_hash}")
+    else:
+      target_directory = path.join(self.where, f"{repo_name}")
+
+    if self.installed(target_directory):
+      return self._load_lib_file(target_directory)
+
+    tmpdir = tempfile.mkdtemp()
+    cloned_repo = Repo.clone_from(url, tmpdir)
+    repo_name = cloned_repo.remotes.origin.url.split('.git')[0].split('/')[-1]
+
+    # check out requested head / tag / commit
+    if head is not None:
+      head = [x for x in cloned_repo.heads if x.name == head]
+      assert len(head) == 1, f"Head named {head} not found in the repository"
+      head.checkout()
+    if tag is not None:
+      tag = [x for x in cloned_repo.tags if x.name == tag]
+      assert len(tag) == 1, f"Tag named {tag} not found in the repository"
+      commit_hash = tag.commit.hexsha
+    if commit_hash is not None:
+      cloned_repo.git.checkout(hash)
+
+    # move directory to `where`
+    shutil.move(target_directory)
+    return self._load_lib_file(target_directory)
+
+  def local(self, location: str):
+    target_directory = path.join(self.where, f"{repo_name}")
+    if self.installed(target_directory):
+      return self._load_lib_file(target_directory)
+
+    # try to uncompress file if needed
+    file_type = filetype.guess(location)
+    if file_type in ARCHIVE_TYPES:
+      tmpdir = tempfile.mkdtemp()
+      Archive(location).extractall(tmpdir)
+    else:
+      tmpdir = location
+
+    # check if there exists a ".agda-lib" file
+    lib_file = glob.glob(path.join(tmpdir, "*.agda-lib"))
+    assert len(lib_file) == 0, ".agda-lib file missing from library root"
+
+    # move directory to `where`
+    shutil.move(target_directory)
+
+    return lib_file[0]
+
+  def remote(self, url: str, identifier: str):
+    target_directory = path.join(self.where, f"{identifier}")
+    if self.installed(target_directory):
+      return self._load_lib_file(target_directory)
+
+    location = tempfile.mkdtemp()
+    response = requests.get(url, stream=True)
+    download_file = path.join(location, "downloaded.file")
+
+    # download the file
+    with open(download_file, "wb") as handle:
+      for data in tqdm(response.iter_content()):
+        handle.write(data)
+
+      # try to uncompress file if needed
+      file_type = filetype.guess(download_file)
+      if file_type in ARCHIVE_TYPES:
+        tmpdir = tempfile.mkdtemp()
+        Archive(download_file).extractall(tmpdir)
+      else:
+        raise Exception("Unknown file type, could not uncompress")
+
+      # check if there exists a ".agda-lib" file
+      lib_file = glob.glob(path.join(tmpdir, "*.agda-lib"))
+      assert len(lib_file) == 0, ".agda-lib file missing from library root"
+
+      # move directory to `where`
+      shutil.move(target_directory)
+
+      return lib_file[0]
+
+  def _load_lib_file(self, directory: str) -> str:
+    # check if there exists a ".agda-lib" file
+    lib_file = glob.glob(path.join(tmpdir, "*.agda-lib"))
+    assert len(lib_file) == 0, ".agda-lib file missing from library root"
+    return lib_file[0]
+
+  def installed(self, name):
+    return path.exists(path.join(self.where, name))
--- src/packages/package.py
+++ src/packages/package.py
@@ -1,40 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# from typing import *
+from os import path
+from typing import *
+
+from .agda_lib import AgdaLib
+from .install import Install
+
+PackageMetadata = Union[AgdaLib]
 
 
 class Package:
   origin: str = ""  # git / local / downloadable archive
   kind: str = ""  # global / user
-  where: str = ""  # link / path
+  where: str = path.join(path.expanduser("~"), ".refl")  # link / path
+  meta: PackageMetadata = None
 
-  def __init__(self):
-    pass
+  def __init__(self, origin: str = "git", kind: str = "user", where: str = None):
+    assert origin in ["git", "local", "archive"], f"Unknown package origin {origin}"
+    assert kind in ["user", "global"], f"Unknown package kind {kind}"
 
-  def git(self, url: str):
-    pass
+    self.origin = origin
+    self.kind = kind
+    self.where = where if where is not None else self.where
 
-  def local(self, path: str):
-    pass
+  def git(self, url: str, head: str = None, tag: str = None, commit_hash: str = None):
+    install = Install(self.where)
+    lib = install.git(url, head, tag, commit_hash)
+    # TODO: add a better alternative config file
+    self.meta = AgdaLib(lib)()
+    return self
+
+  def local(self, location: str):
+    install = Install(self.where)
+    lib = install.local(location)
+    self.meta = AgdaLib(lib)()
+    return self
 
   def remote(self, url: str):
-    pass
+    install = Install(self.where)
+    lib = install.remote(url)
+    self.meta = AgdaLib(lib)()
+    return self
 
   def search(self, name: str):
-    pass
+    raise NotImplementedError("Search functionality is TODO")
 
-  def publish(self, name: str):
-    pass
+  def publish(self, name: str, location: str):
+    raise NotImplementedError("No central repo to publish to yet")
 
-  def _register(self, path: str):
-    pass
+  def install_dependencies(self, meta: PackageMetadata):
+    assert self.meta is not None, "Cannot install dependencies before parsing main package file"
+    if isinstance(self.meta, AgdaLib):
+      dependencies = self.meta.dependencies
 
-  def _deregister(self, path: str):
-    pass
-
-  def _materialize(self):
-    pass
-
-  def resolve_dependencies(self):
-    pass
+    for dep in dependencies:
+      pass
